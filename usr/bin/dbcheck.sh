5c77b6f511b1bb04b4659d891c574e87471f38cf
David Klann <dklann@linux.com>
2016-01-25 11:43:47 -0600

    Refactor, convert to ZSH, use external doSQL() from zsh-functions, add logging.

--- usr/bin/dbcheck.sh
+++ usr/bin/dbcheck.sh
@@ -1,102 +1,92 @@
-#!/bin/bash
+#!/bin/zsh
 
-# What we need the script to do...
-#
-# -virtual machine 'rd-service' gets autostarted by libvirt on boot
-#
-# -AFTER jackd AND AFTER the DB is accessible we need to start rivendell
-# daemons on the host.
-#
-# -AFTER the DB is accessible AND rivendell HOST daemons are running we
-# start AIRPLAY VMs
-#
-# -AFTER the DB is accessible we start MANAGEMENT VMs
-#
-# hope that makes sense ;)
-#
-# I think there should be a good way to use systemd service files to do
-# this along with a script that sets an environment var as a flag, or the
-# above service files are dependent on the service file that launches the
-# script to exit first.
+##############################################################################
+##############################################################################
+##
+## dbcheck.sh: ensure that the Rivendell database is accessible before
+## exiting this script. This is used by the various systemd service
+## scripts in rivendell.target.
+##
+############################################################
+### DO NOT USE THIS SCRIPT WITHOUT MAKING MODIFICATIONS! ###
+############################################################
+##
+## NOTE:
+## This script is COMPLETELY installation-specific! See the "BUG ALERT" below.
+##
+##############################################################################
+##############################################################################
 
-# Read a .ini-style configuration file and return the value for the
-# parameter passed. It's a hack, but it works.
-getConfValue() {
-    local confFile=${1:?"Please name a config file to read."} && shift
-    local section=${1:?"Please name a config file section to read."} && shift
-    local parm=${1:?"What parameter from the config file ${confFile}?"} && shift
-    local returnValue=1
+# log STDOUT and STDERR of this script and all commands called by this script to separate files
+exec 1> /var/tmp/${0##*/}.out
+exec 2> /var/tmp/${0##*/}.err
 
-    while read line
-    do
-	# see if the line is a "section" specifier
-	expr match "${line}" "\[${section}\]" > /dev/null && { inSection=1 ; continue ; }
+RM=$(whence rm) ; RM=${RM:-${ROOT:-/}bin/rm}
+LN=$(whence ln) ; LN=${LN:-${ROOT:-/}bin/ln}
+LOGGER=$(whence logger) ; LOGGER=${LOGGER:-${ROOT:-/}usr/bin/logger}
 
-	if (( inSection ))
-	then
-	    # rivendell uses parm=value (spaces not permitted, I think, but I'll check for them anyway)
-	    if expr match "${line}" "${parm}[[:space:]]*=.*" > /dev/null
-	    then
-		# this eval() strips leading and trailing whitespace
-		# from the variable too.
-		value="$(eval echo ${line#*=})"
-		returnValue=0
-		break
-	    fi
-	fi
-    done < ${confFile}
+if [[ -f ${ROOT:-/}usr/local/bin/zsh-functions ]] ; then
+    if [[ -r ${ROOT:-/}usr/local/bin/zsh-functions ]] ; then
+	source ${ROOT:-/}usr/local/bin/zsh-functions
+    else
+	${LOGGER} --stderr -t ${0##*/} -p local7.err -i "ERROR: Cannot read '/usr/local/bin/zsh-functions' (${?}). Check permissions."
+    fi
+else
+    ${LOGGER} --stderr -t ${0##*/} -p local7.err -i "ERROR: Cannot find '/usr/local/bin/zsh-functions' (${?})."
+fi
 
-    echo "${value}"
+##
+## Theory of Operation
+##
+## - virtual machine 'rd-service' gets autostarted by libvirt on boot
+##
+## - AFTER jackd is running, AND AFTER the database is accessible we
+## need to start rivendell daemons on the host.
+##
+## - AFTER the database is accessible AND rivendell HOST daemons are
+## running we start AIRPLAY VM(s)
+##
+## - AFTER the database is accessible we start MANAGEMENT VM(s)
+##
+## See also the systemd service files and their associated
+## dependencies in /etc/systemd/system/rivendell.target.wants
+##
 
-    return ${returnValue}
-}
-
-# Return true (Zero) if we can make a valid connection to the remote
-# database and retrieve a count of carts in the CART table.
+##
+## rbRunning()
+## Return true (Zero) if we can make a valid connection to the remote
+## database and retrieve a count of carts in the CART table.
+##
 dbRunning() {
     local cartCount=0 returnValue=1
 
-    local dbHost=${DB_HOST:-$(getConfValue ${rdConf} mySQL Hostname)}
-    local dbUser=${DB_USER:-$(getConfValue ${rdConf} mySQL Loginname)}
-    local dbPass=${DB_PASS:-$(getConfValue ${rdConf} mySQL Password)}
-    local db=${DB:-$(getConfValue ${rdConf} mySQL Database)}
-
-    cartCount=$(mysql -s -N -h "${dbHost}" -u "${dbUser}" -p"${dbPass}" ${db} -e "SELECT COUNT(*) FROM CART")
-    logger -i -t ${0##*/} -p local7.info "Found ${cartCount} CARTs in the ${db} database."
+    cartCount=$(doSQL "SELECT COUNT(*) FROM CART")
+    ${LOGGER} -t ${0##*/} -p local7.info -i "Found ${cartCount} CARTs in the ${db} database."
 
     (( cartCount )) && returnValue=0
 
     return ${returnValue}
 }
 
-rdConf=${RD_CONF:-${ROOT:-/}etc/rd.conf}
-dbHost=${DB_HOST}
 maxAttempts=${MAX_ATTEMPTS:-100}
 sleepTime=${SLEEP_TIME:-1}
 exitValue=1
 
-if [[ -f ${rdConf} ]]
+loopCount=1
+while :
+do
+    # wait for the database to be started before proceeding further
+    dbRunning && { exitValue=0 ; break ; }
+
+    (( loopCount++ > maxAttempts )) && break
+
+    sleep ${sleepTime}
+done
+
+if (( loopCount > maxAttempts ))
 then
-
-    loopCount=1
-    while :
-    do
-	# wait for the database to be started before proceeding further
-	dbRunning && { exitValue=0 ; break ; }
-
-	(( loopCount++ > maxAttempts )) && break
-
-	sleep ${sleepTime}
-    done
-
-    if (( loopCount > maxAttempts ))
-    then
-	logger -i -s -t ${0##*/} -p local7.error "ERROR: Tried ${loopCount} times to start the RAM systems."
-	exitValue=9
-    fi
-else
-    logger -i -s -t ${0##*/} -p local7.error "ERROR: Could not find ${rdConf} configuration. Is Rivendell installed?"
-    exitValue=3
+    ${LOGGER} --stderr -t ${0##*/} -p local7.error -i "ERROR: Tried ${loopCount} times to start the RAM systems."
+    exitValue=9
 fi
 
 exit ${exitValue}
